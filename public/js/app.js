6e0fd401cc65fe948e6fea161f255d3210e3ce98
Luchanso <luchansodev@gmail.com>
2016-11-01 20:57:00 +0300

    Update

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,10 @@
 // const id = 210700286
 // const verified = true
+var score = 0
 
 function centeringPage() {
-  const app = $('#app')
-  const marginTop = window.innerHeight / 2 - app.height() / 2
+  const app = $('#centering-anchor')
+  const marginTop = window.innerHeight / 2 - app.height()
 
   app.css('margin-top', marginTop)
 }
@@ -13,14 +14,37 @@ function addEvents() {
 }
 
 function agragate() {
+  showCounter()
   const id = preapreId($('#enter-id').val())
 
   var socket = io.connect('/', {transports: ['websocket']})
-  socket.emit('id', id)
+  socket.emit('getRegistrationDate', id)
+  socket.on('getRegistrationDate', userRegistrationDate)
+}
+
+function getVkData(id) {
+  VK.api('users.get', {
+    user_ids: [id],
+    fields: 'city,verified'
+  }, (data) => {
+
+  })
+}
+
+function userRegistrationDate(date) {
+  const min = Date.now() - (new Date("2006-09-23T20:26:12+03:00")).getTime()
+
+  let timespan = Date.now() - new Date(date).getTime()
+
+  score += 100 * (timespan / min)
 }
 
 function preapreId(str) {
   return +str
+}
+
+function showCounter() {
+  $('.counter').css('visibility', 'inherit')
 }
 
 centeringPage()
--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 
 const server = require('http').Server(app)
-const io = require('socket.io')(server) // , { wsEngine: 'uws' }
+const io = require('socket.io')(server, { wsEngine: 'uws' })
 
 app.set('port', process.env.PORT || 8080)
 
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/../public'))
 io.on('connection', (socket) => {
   let user
 
-  // socket.on('id', newId)
+  socket.on('getRegistrationDate', newId)
 })
 
 // Not work with id59967447
@@ -25,10 +25,10 @@ function newId(id) {
       user.parseData()
     })
     .then(() => {
-      console.log(user.followers, user.friends, user.registration)
+      socket.emit("getRegistrationDate", user.registration)
     })
     .catch(err => {
-      console.error(err)
+      console.log(err)
     })
 }
 
