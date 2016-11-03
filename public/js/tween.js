be4977dea4c8dee140ef4ec3f2225aa50ca6f1e8
Luchanso <luchansodev@gmail.com>
2016-11-03 18:53:23 +0300

    Update

---
+++ public/js/tween.js
@@ -0,0 +1,10 @@
+var tween = new TWEEN.Tween(window)
+    .to({ score: val }, 1000)
+    .start();
+// 
+// requestAnimationFrame(animate);
+//
+// function animate(time) {
+//     requestAnimationFrame(animate);
+//     TWEEN.update(time);
+// }
